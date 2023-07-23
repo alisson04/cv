f48b54bcd868bd1bb83988867660ed3e5c553355
Alisson Fernandes <alisson.vinicius@cohros.com.br>
2023-07-23 00:32:50 -0300

    [bug] fixed menu and changed layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { List, ListItem } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
-import { HashtagIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-  const [selected, setSelected] = React.useState(1);
+  const location = useLocation();
+  const locationIndex = {'/': 0, '/skills': 1};
+  const [selected, setSelected] = React.useState(locationIndex[location.pathname]);
   const setSelectedItem = (value) => setSelected(value);
   const menuItems = [{name: 'HOME', path: '/'}, {name: 'SKILLS', path: '/skills'}];
 
   return (
     <nav>
-      <List className="text-white gap-0">
+      <List className="gap-0">
         {menuItems.map((item, index) => (
           <Link key={index} to={item.path}>
             <ListItem
               className={
                 'bg-transparent rounded-none border-gray-700 ' +
-                'hover:bg-transparent hover:pl-6 hover:border-b hover:border-t hover:text-cv-color-2 ' +
-                'active:bg-transparent active:border-b active:border-t active:text-cv-color-2 active:text-cv-color-2 ' +
-                'focus:bg-transparent focus:border-b focus:border-t focus:text-cv-color-2 focus:text-cv-color-2 ' +
-                (selected === index ? 'text-cv-color-2' : 'text-cv-color-1')
+                'hover:bg-transparent hover:pl-6 hover:border-b hover:border-t ' +
+                'active:bg-transparent active:text-white focus:text-white focus:bg-transparent ' +
+                (selected === index ? 'border-t border-b text-white' : '')
               }
               selected={selected === index}
               onClick={() => setSelectedItem(index)}
             >
-              {selected === index && <HashtagIcon strokeWidth={2} className="h-5 w-5" />}
+              {selected === index && <ChevronLeftIcon strokeWidth={2} className="h-5 w-5" />}
               {item.name}
+              {selected === index && <ChevronRightIcon strokeWidth={2} className="h-5 w-5" />}
             </ListItem>
           </Link>
         ))}
